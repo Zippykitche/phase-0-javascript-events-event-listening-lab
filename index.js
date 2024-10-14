7e888ef66ed04ac47ff15fd3db4d2fafab532d3b
zippykitche <zippyk80@gmail.com>
2024-10-14 15:11:19 +0300

    event listener lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+  input.addEventListener('click', () => {
+    alert ('I was clicked');
+  });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    addingEventListener();
+  });
+
